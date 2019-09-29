9355be437ae72e41c501fbe2dc3234831a815232
zhou.wei <zhou.wei@qiaofang.cn>
2019-09-29 15:11:04 +0800

    init

--- src/main/java/com/sapling/common/tools/collection/ArrayUtil.java
+++ src/main/java/com/sapling/common/tools/collection/ArrayUtil.java
@@ -1,9 +1,10 @@
 package com.sapling.common.tools.collection;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
+ * list util
+ *
  * @author weizhou
  * @version v1.0
  * @date 2018/7/2
@@ -11,12 +12,23 @@ import java.util.List;
  */
 public class ArrayUtil {
 
+    /**
+     * Transfer list to string, with separator to join in each item
+     *
+     * @param arrays
+     * @param join
+     * @return
+     */
     public static String join(List<Object> arrays, String join) {
         StringBuilder sb = new StringBuilder();
         arrays.forEach(item -> sb.append(String.valueOf(item)).append(join));
-        return sb.substring(0,sb.length()-1);
+        return sb.substring(0, sb.length() - 1);
     }
 
-
+    public static Object[] listToArray(List<?> list) {
+        Object[] ret = new Object[list.size()];
+        ret = list.toArray(ret);
+        return ret;
+    }
 
 }
